88f808dd081076e66de1c3426135d846f0f9b7ce
WolfgangDeigele <wolfgang.deigele@tum.de>
2020-10-05 12:05:05 +0200

    Changed line ending encoding

--- SDDI-CKAN-Docker source files/contrib/docker/ckan-entrypoint.sh
+++ SDDI-CKAN-Docker source files/contrib/docker/ckan-entrypoint.sh
@@ -62,6 +62,7 @@ if [ -z "$CKAN_DATAPUSHER_URL" ]; then
     abort "ERROR: no CKAN_DATAPUSHER_URL specified in docker-compose.yml"
 fi
 
+
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
 exec "$@"
